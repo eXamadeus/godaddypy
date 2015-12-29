04fd573e5b18e4ba302d3b9e5bbb6f0e6ec988ec
Julian Coy <julian.coy@sparcedge.com>
2015-12-28 20:04:54 -0500

    Moved version number to a better location in the API URL strings

--- godaddypy/godaddypy.py
+++ godaddypy/godaddypy.py
@@ -6,14 +6,14 @@ _logger = logging.getLogger(__name__)
 
 
 class GoDaddyAPI(object):
-    _API_TEMPLATE = 'https://api.godaddy.com'
+    _API_TEMPLATE = 'https://api.godaddy.com/v1'
 
-    _GET_DOMAINS = '/v1/domains'
-    _GET_DOMAIN = '/v1/domains/{domain}'
-    _GET_RECORDS_TYPE_NAME = '/v1/domains/{domain}/records/{type}/@'
-    _PUT_RECORDS_TYPE_NAME = '/v1/domains/{domain}/records/{type}/{name}'
-    _PATCH_RECORDS = '/v1/domains/{domain}/records'
-    _PUT_RECORDS = '/v1/domains/{domain}/records'
+    _GET_DOMAINS = '/domains'
+    _GET_DOMAIN = '/domains/{domain}'
+    _GET_RECORDS_TYPE_NAME = '/domains/{domain}/records/{type}/@'
+    _PUT_RECORDS_TYPE_NAME = '/domains/{domain}/records/{type}/{name}'
+    _PATCH_RECORDS = '/domains/{domain}/records'
+    _PUT_RECORDS = '/domains/{domain}/records'
     _SSO_KEY_TEMPLATE = 'sso-key {api_key}:{api_secret}'
 
     def discover_domains(self, account):
